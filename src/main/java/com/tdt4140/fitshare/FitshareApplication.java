11c0fa2814a9e78c0084d4630ed9d47a32f5b442
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-08 11:26:17 +0100

    1.1 downloaded project from spring initializr Dep: Lombok, Spring Web, Spring DevTools, Spring mongoDB

---
+++ src/main/java/com/tdt4140/fitshare/FitshareApplication.java
@@ -0,0 +1,14 @@
+package com.tdt4140.fitshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitshareApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(FitshareApplication.class, args);
+	}
+
+}
