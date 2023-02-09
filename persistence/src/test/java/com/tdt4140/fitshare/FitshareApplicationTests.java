72750e591d65855424d460b8d0430591e26c96b1
Sverre Nystad <sverrnys@ntnu.no>
2023-02-09 22:49:35 +0100

    1.2 Add test for appliaction

---
+++ persistence/src/test/java/com/tdt4140/fitshare/FitshareApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tdt4140.fitshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
