717707c1be30352d01429b16df0035d23e04f1d8
Sverre Nystad <sverrnys@ntnu.no>
2023-02-09 22:48:40 +0100

    1.2 add basic spring classes for handling http requests

--- src/main/java/com/tdt4140/fitshare/ServerApplication.java
+++ src/main/java/com/tdt4140/fitshare/ServerApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FitshareApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		SpringApplication.run(FitshareApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/tdt4140/fitshare/ServerService.java
@@ -0,0 +1,11 @@
+package com.tdt4140.fitshare;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServerService {
+
+	public String getHelloWorld() {
+		return "Hello world";
+	}
+}
