2fa9f0f297567763ed0077aad0a415466a710d79
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-15 15:58:38 +0100

    16.3 Changed put to post

--- src/main/java/com/pu/fitshare/server/ServerController.java
+++ src/main/java/com/pu/fitshare/server/ServerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -66,9 +67,9 @@ public class ServerController {
     }
 
 
-    
 
-    @PutMapping(path = "/users/signup/{username}/{password}")
+
+    @PostMapping(path = "/users/signup/{username}/{password}")
     public ResponseEntity<User> putUser(@PathVariable("username") String username,
             @PathVariable("password") String password) {
 
