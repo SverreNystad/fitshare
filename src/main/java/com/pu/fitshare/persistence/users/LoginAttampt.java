2d145d498719fb825c277eb981361f1a70be254c
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-15 12:15:02 +0100

    16.1 Created User and LogiAttempt

---
+++ src/main/java/com/pu/fitshare/persistence/users/LoginAttampt.java
@@ -0,0 +1,27 @@
+package com.pu.fitshare.persistence.users;
+
+/**
+ * LoginAttampt will take the input from the http request and sanitize it
+ * before letting it pass futher.
+ */
+public class LoginAttampt {
+	private String username;
+	private String password;
+
+	public LoginAttampt(final String username, final String password) throws IllegalArgumentException {
+		sanitize(username, password);
+		this.username = username;
+		this.password = password;
+	}
+
+	/**
+	 * Make sure there are no malicious input.
+	 * 
+	 * @param username to check for bad input
+	 * @param password to check for bad input
+	 * @throws IllegalArgumentException when there are bad input
+	 */
+	private void sanitize(final String username, final String password) throws IllegalArgumentException {
+		// TODO: must do tests on input
+	}
+}
---
+++ src/main/java/com/pu/fitshare/persistence/users/User.java
@@ -0,0 +1,14 @@
+package com.pu.fitshare.persistence.users;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+	@Id
+	private String id;
+
+	private String firstName;
+	private String lastName;
+	private String epost;
+	private String description;
+
+}
