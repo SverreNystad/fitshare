07662eb8894f1ea14466aaad8108966bcb7e3316
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-15 13:01:56 +0100

    16.1 Make user class like db document

--- src/main/java/com/pu/fitshare/persistence/users/User.java
+++ src/main/java/com/pu/fitshare/persistence/users/User.java
@@ -1,26 +1,32 @@
 package com.pu.fitshare.persistence.users;
 
-import java.util.Date;
+// import java.util.Date;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "user")
 public class User {
     @Id
-    private int id;
-    private String firstName;
-    private String lastName;
+    private ObjectId id;
+    private String name;
+    private String password;
+
+    // private String firstName;
+    // private String lastName;
     
-    private Date birthday;
+    // private Date birthday;
     // private String description;
     // private List<TrainingGoal> goals;
 
 
-    public User(final int id, final String firstName, final String lastName, final Date birthday) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthday = birthday;
-    }
+    // public User(final int id, final String firstName, final String lastName, final Date birthday) {
+    //     this.id = id;
+    //     this.firstName = firstName;
+    //     this.lastName = lastName;
+    //     this.birthday = birthday;
+    // }
 
 
 }
