bf7d01a4b7047d02776088d0758f924026bffcc8
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2023-02-16 15:30:55 +0100

    added styling for navbar
    imported sass to make css less painful

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../img/logo.png";
+import style from "./Nav.module.scss";
 
 export default function Nav() {
   return (
-    <nav>
-      <div>
-        <Link to={"/"}>
+    <nav id="navigation">
+      <div className={style.navContainer}>
+        <Link className={style.logo} to={"/"}>
           <img width="42px" src={logo} alt="Logo" />
+          <span>FitShare</span>
         </Link>
-        <span>FitShare</span>
+        <ul className={style.ul}>
+          <li className={style.li}>
+            <Link to={"/profile"}>Profil</Link>
+          </li>
+          <li className={style.li}>
+            <Link to={"/friends"}>Venner</Link>
+          </li>
+          <li className={style.li}>
+            <Link to={"/groups"}>Grupper</Link>
+          </li>
+          <li className={style.li}>
+            <Link to={"/plans/new"}>Ny trening</Link>
+          </li>
+        </ul>
+        <div>
+          <span className={style.shortcut}>Snarveier</span>
+          <ul className={style.ul}>
+            <li className={style.li}>
+              <Link to={"#"}>Progresjon</Link>
+            </li>
+            <li className={style.li}>
+              <Link to={"/plans"}>Tidligere økter</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul>
-        <li>
-          <Link to={"/profile"}>Profil</Link>
-        </li>
-        <li>
-          <Link to={"/friends"}>Venner</Link>
-        </li>
-        <li>
-          <Link to={"/groups"}>Grupper</Link>
-        </li>
-        <li>
-          <Link to={"/plans/new"}>Ny trening</Link>
-        </li>
-      </ul>
-      <span>Snarveier</span>
-      <ul>
-        <li>
-          <Link to={"#"}>Progresjon</Link>
-        </li>
-        <li>
-          <Link to={"/plans"}>Tidligere økter</Link>
-        </li>
-      </ul>
     </nav>
   );
 }
