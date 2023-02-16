d611a5de2536027fd4633f14ac05e892b2b3ff72
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-16 18:23:11 +0100

    7.3 Add more to TrainingGoal and create History

---
+++ src/main/java/com/pu/fitshare/persistence/training/History.java
@@ -0,0 +1,17 @@
+package com.pu.fitshare.persistence.training;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+
+/** 
+ * The {@code History} class shall keep all the progression and keep the dates with the entries.
+ * 
+*/
+public class History {
+	private Map<Date, Integer> history;
+
+	public Collection<Integer> getProgression() {
+		return history.values();
+	}
+}
