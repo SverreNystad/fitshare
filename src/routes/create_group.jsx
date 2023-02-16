213d5a8d819adcf4c1ef4ffbe0045c4594d81d4b
vidarslu <vidarslu@stud.ntnu.no>
2023-02-16 14:01:52 +0100

    La til ikoner for knappene

--- src/routes/create_group.jsx
+++ src/routes/create_group.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import style from "./create_group.module.css"
-import img1 from "../img/group.png"
+import group from "../img/group.png"
+import bike from "../img/bike.png"
+import swim from "../img/swim.png"
+import weight from "../img/weight.png"
+import shoe from "../img/shoe.png"
 
 export default function Create_Group() {
   return (
@@ -18,11 +22,11 @@ export default function Create_Group() {
         
         
         <div>
-        <button type="button" className={style.dot}>Group</button>
-        <button type="button" className={style.dot}>Weight</button>
-        <button type="button" className={style.dot}>Shoe</button>
-        <button type="button" className={style.dot}>Bike</button>
-        <button type="button" className={style.dot}>Swim</button>
+        <button type="button" className={style.dot}><img src={group} alt="Community" className={style.icon} /></button>
+        <button type="button" className={style.dot}><img src={weight} alt="Strength" className={style.icon} /></button>
+        <button type="button" className={style.dot}><img src={shoe} alt="Running" className={style.icon} /></button>
+        <button type="button" className={style.dot}><img src={bike} alt="Cycling" className={style.icon} /></button>
+        <button type="button" className={style.dot}><img src={swim} alt="Swimming" className={style.icon} /></button>
         </div>
         <button type="submit" className={style.submitfield}>OPPRETT</button>
         </form>
