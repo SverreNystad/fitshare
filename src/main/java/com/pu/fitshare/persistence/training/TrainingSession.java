edbd598b3cd209e2ea501eef7d7d40ba5b7cd849
LockedInTheSkage <LockedInTheSkage@users.noreply.github.com>
2023-02-17 22:48:59 +0100

    #7.6 Added TrainingSession and TrainingExercise

--- src/main/java/com/pu/fitshare/persistence/training/TrainingSession.java
+++ src/main/java/com/pu/fitshare/persistence/training/TrainingSession.java
@@ -1,11 +1,17 @@
 package com.pu.fitshare.persistence.training;
 
+import java.util.ArrayList;
+
 import org.bson.types.ObjectId;
 
 public class TrainingSession {
 
+    private ObjectId _id;
+    private Integer duration;
+    private ArrayList<TrainingExercise> exercises;
+
     public boolean idEquals(ObjectId id) {
-        return false;
+        return (_id==id);
     }
 
 }
--- src/main/java/com/pu/fitshare/persistence/users/UserRepository.java
+++ src/main/java/com/pu/fitshare/persistence/users/UserRepository.java
@@ -2,6 +2,7 @@ package com.pu.fitshare.persistence.users;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 /**
