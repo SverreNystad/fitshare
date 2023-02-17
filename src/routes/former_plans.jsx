92f3213a4cc1b29a86290ee24ade37db3d8ec808
eirhels <eirhels@stud.ntnu.no>
2023-02-17 11:49:49 +0100

    Started to create design, need more adjustments

--- src/routes/former_plans.jsx
+++ src/routes/former_plans.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import style from "./former_plans.module.css"
-import image from "../img/logo.png"
-import { Link } from "react-router-dom";
+import weight from "../img/weight.png"
+import shoe from "../img/shoe.png"
+import bike from "../img/bike.png"
+import swim from "../img/swim.png"
 
 export default function Plans() {
   return (
     <>
-      <main className={style.signin}>
-      <img className={style.image} src={image} alt=""/>
-      <h1 className={style.headline}>Registrer deg</h1>
+      <main className={style.former_plans}>
+      <h1 className={style.headline}>Tidligere økter</h1>
       <form method="post" className={style.form}>
-      <input type="text" placeholder="Brukernavn" className={style.field}/>
-      <input type="password" placeholder="Passord" className={style.field}/>
-      <input type="password" placeholder="Bekreft passord" className={style.field}/>
-      <button type="submit" className={style.submitbutton}>Registrer deg</button>
-      <Link  to={"/login"} className={style.button}>Allerede registrert? Logg inn her</Link>
+      <div className={style.rows}>
+      <img src={weight} alt="Strength" className={style.icon}/><button type="text" className={style.field}> Styrketrening </button>
+      </div>
+      <div className={style.rows}>
+      <img src={shoe} alt="Running" className={style.icon}/><button type="text" className={style.field}> Kondisjon </button>
+      </div>
+      <div className={style.rows}>
+      <img src={bike} alt="Cycling" className={style.icon}/><button type="text" className={style.field}> Svømming </button>
+      </div>
+      <div className={style.rows}>
+      <img src={swim} alt="Swimming" className={style.icon}/><button type="text" className={style.field}> Sykling </button>
+      </div>
       </form>
       </main>
     </>
