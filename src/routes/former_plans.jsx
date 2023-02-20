0534e40bc4bf9bb480f884ba026d1685311b35dd
eirhels <eirhels@stud.ntnu.no>
2023-02-20 14:36:39 +0100

    Co-authored-by: Martine Nilsen <martin39@stud.ntnu.no>

--- src/routes/former_plans.jsx
+++ src/routes/former_plans.jsx
@@ -4,26 +4,27 @@ import weight from "../img/weight.png"
 import shoe from "../img/shoe.png"
 import bike from "../img/bike.png"
 import swim from "../img/swim.png"
+import { Link } from "react-router-dom";
 
 export default function Plans() {
   return (
     <>
       <main className={style.former_plans}>
-      <h1 className={style.headline}>Tidligere økter</h1>
-      <form method="post" className={style.form}>
-      <div className={style.rows}>
-      <img src={weight} alt="Strength" className={style.icon}/><button type="text" className={style.field}> Styrketrening </button>
-      </div>
-      <div className={style.rows}>
-      <img src={shoe} alt="Running" className={style.icon}/><button type="text" className={style.field}> Kondisjon </button>
-      </div>
-      <div className={style.rows}>
-      <img src={bike} alt="Cycling" className={style.icon}/><button type="text" className={style.field}> Svømming </button>
-      </div>
-      <div className={style.rows}>
-      <img src={swim} alt="Swimming" className={style.icon}/><button type="text" className={style.field}> Sykling </button>
-      </div>
-      </form>
+        <h1 className={style.headline}>Tidligere økter</h1>
+        <form method="post" className={style.form}>
+        <div className={style.rows}>
+          <img src={weight} alt="Strength" className={style.icon}/><Link to={"/plans/strength"} className={style.field}> Styrketrening </Link>
+        </div>
+        <div className={style.rows}>
+          <img src={shoe} alt="Running" className={style.icon}/><Link to={"/plans/running"} className={style.field}> Løping </Link>
+        </div>
+        <div className={style.rows}>
+          <img src={bike} alt="Cycling" className={style.icon}/><Link to={"/plans/cycling"} className={style.field}> Sykling </Link>
+        </div>
+        <div className={style.rows}>
+          <img src={swim} alt="Swimming" className={style.icon}/><Link to={"/plans/swimming"} className={style.field}> Svømming </Link>
+        </div>
+        </form>
       </main>
     </>
   );
