f4976afde63c652c4714902aca9f310a64cdb792
martin39 <martin39@stud.ntnu.no>
2023-02-20 16:00:43 +0100

    started on the login-endpoint

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -3,11 +3,26 @@ import style from "./login.module.scss";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+
+  async function postLogInData(event, url="api/v1/users/login/{username}/{password}"){
+    event.preventDefault();
+    console.log(event.target[1].value);
+    let username = event.target[0].value;
+    let password = event.target[1].value;
+
+    const response = await fetch(`http://localhost:8080/api/v1/users/login/${username}/${password}`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'no-cors',
+    })
+    console.log(response);
+  }
+
+
   return (
     <>
       <main className={style.login}>
         <h1 className={style.headline}>Logg inn</h1>
-        <form method="post" className={style.form}>
+        <form onSubmit={postLogInData} className={style.form}>
           <input type="text" placeholder="Brukernavn" className={style.field} />
           <input
             type="password"
