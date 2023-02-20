15225f1d2876b78873f10efb5d65399f9249883c
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-20 16:32:45 +0100

    #9 Add CrossOrigin to controllers

--- src/main/java/com/pu/fitshare/server/controllers/ServerController.java
+++ src/main/java/com/pu/fitshare/server/controllers/ServerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.pu.fitshare.models.users.User;
 import com.pu.fitshare.server.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ServerController {
 
     @Autowired
