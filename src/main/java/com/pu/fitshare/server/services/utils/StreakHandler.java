ae53d2e758e73432b32829fd9064e94318ac8ae9
Sverre Nystad <sverrnys@ntnu.no>
2023-03-05 00:28:45 +0100

    #28 Make StreakHandler and test

---
+++ src/main/java/com/pu/fitshare/server/services/utils/StreakHandler.java
@@ -0,0 +1,35 @@
+package com.pu.fitshare.server.services.utils;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import com.pu.fitshare.models.users.User;
+
+public class StreakHandler {
+
+	/*
+	 * If the difference is larger than 2 days set streak to 0
+	 * If the difference is larger than 1 then add day to streak
+	 */
+
+	public static User handleStreak(User u) {
+		Date currenDate = new Date();
+		Date lastLogin = u.getLastLogin();
+		int daysSinceLastLogin = findDifferenceInDays(currenDate, lastLogin);
+
+		if (daysSinceLastLogin >= 1) {
+			u.setStreak(u.getStreak() + 1);
+		}
+
+		if (daysSinceLastLogin >= 2) {
+			u.setStreak(0);
+		}
+		return u;
+	}
+
+	public static int findDifferenceInDays(final Date firstDate, final Date secondDate) {
+		long diffInMillies = Math.abs(secondDate.getTime() - firstDate.getTime());
+		long differenceInDays = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+		return (int) differenceInDays;
+	}
+}
