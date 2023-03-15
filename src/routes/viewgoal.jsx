57f06c1dd96b8ae571f93a950ad5dbaac54b20a8
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-03-15 11:51:40 +0100

    #55 Add showcase goal

--- src/routes/viewgoal.jsx
+++ src/routes/viewgoal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Radio, Input, Button, Textarea } from "../components/Inputs";
 import style from "./viewgoal.module.scss";
 import bikeImg from "../img/bike.png";
@@ -10,6 +10,7 @@ import { CChartLine } from '@coreui/react-chartjs';
 
 
 export default function NewPlan() {
+  const [showcasedGoal, setShowcasedGoal] = useState({});
   const intensity = [
     { key: 0, name: "intensity", label: "Lett", value: "low" },
     { key: 1, name: "intensity", label: "Meduim", value: "medium" },
@@ -59,45 +60,45 @@ export default function NewPlan() {
     alert(`Laget økten ${res.name}`);
   };
 
-  
-    const chartData = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets:[
-        {
-          label: "Progresjon",
-          backgroundColor: "rgba(0,0,0,0,0)",
-          borderColor: "rgba(220, 220, 220, 1)",
-          pointBackgroundColor: "fff",
-          pointBorderColor: "#fff",
-          data: [60,65,69,75,85,90,95,100],
-  
-        },
-      ],
-    };
-  
+
+  const chartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Progresjon",
+        backgroundColor: "rgba(0,0,0,0,0)",
+        borderColor: "rgba(220, 220, 220, 1)",
+        pointBackgroundColor: "fff",
+        pointBorderColor: "#fff",
+        data: [60, 65, 69, 75, 85, 90, 95, 100],
+
+      },
+    ],
+  };
+
 
   return (
     <div className={style.container}>
       <h1 className={style.header}>[Mål tittel]</h1>
-    
+
       <div className={style.chartAndPR}>
         <CChartLine className={style.chart} type="line" data={chartData} />
 
         <form className={style.PR} onSubmit={handleSubmit}>
-          
+
           <div>Registrer ny PR</div>
           <Input type="text" name="vekt" placeholder="Vekt"></Input>
           <Input type="text" name="reps" placeholder="Reps"></Input>
           <Input type="date" name="dato" placeholder="Dato"></Input>
           <Button type="submit">Registrer</Button>
         </form>
-      
-        
+
+
 
       </div>
       <div className={style.activityForm} onSubmit={handleSubmit}>
-      
-      
+
+
         <Input type="text" name="name" id="name" placeholder="Navn" />
         <Input
           type="number"
