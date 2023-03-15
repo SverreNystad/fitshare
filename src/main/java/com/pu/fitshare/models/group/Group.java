3ce659ebac87dee27dcba7a379c7a3637f3b416c
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2023-03-15 12:22:18 +0100

    added list of sessionIds to group

--- src/main/java/com/pu/fitshare/models/group/Group.java
+++ src/main/java/com/pu/fitshare/models/group/Group.java
@@ -1,5 +1,7 @@
 package com.pu.fitshare.models.group;
 
+import java.util.ArrayList;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.pu.fitshare.models.training.TrainingGoal;
+import com.pu.fitshare.models.training.TrainingSession;
 import com.pu.fitshare.models.training.TrainingType;
 
 import lombok.AllArgsConstructor;
@@ -18,32 +21,52 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Group {
-    
+
     @Id
-    @JsonSerialize(using= ToStringSerializer.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     private String name;
     private TrainingGoal goal;
     private String description;
     private TrainingType type;
+    private ArrayList<ObjectId> sessions;
 
     public Group(final String name, final String description, final String type) {
-      this.name = name;
-      this.description = description;
-      this.goal = new TrainingGoal();
-      this.type = TrainingType.valueOf(type);
-	  }
-    
+        this.name = name;
+        this.description = description;
+        this.goal = new TrainingGoal();
+        this.type = TrainingType.valueOf(type);
+        this.sessions = new ArrayList<ObjectId>();
+    }
+
     public Group(final String name, final TrainingGoal goal) {
-      this.name = name;
-      this.description = "What seems impossible today will one day be your warmup";
-      this.goal = goal;
-	  }
+        this.name = name;
+        this.description = "What seems impossible today will one day be your warmup";
+        this.goal = goal;
+        this.sessions = new ArrayList<ObjectId>();
+    }
 
     public Group(final String nameString) {
-      this.name = nameString;
-      this.description = "What seems impossible today will one day be your warmup";
-      this.goal = new TrainingGoal();
-	  }
+        this.name = nameString;
+        this.description = "What seems impossible today will one day be your warmup";
+        this.goal = new TrainingGoal();
+        this.sessions = new ArrayList<ObjectId>();
+    }
+
+    public Boolean isSession(final String id) {
+        ObjectId sessionId = new ObjectId(id);
+        boolean isInSessions = false;
+        for (ObjectId session : sessions) {
+            if (sessionId.equals(session)) {
+                isInSessions = true;
+            }
+        }
+        return isInSessions;
+    }
+
+    public void addSession(final String id) {
+        ObjectId sessionId = new ObjectId(id);
+        this.sessions.add(sessionId);
+    }
 
 }
