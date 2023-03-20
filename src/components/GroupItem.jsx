3167c8bb999eaf787273ea4b397c130366d69575
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2023-03-20 15:42:42 +0100

    clean up group item

--- src/components/GroupItem.jsx
+++ src/components/GroupItem.jsx
@@ -1,9 +1,16 @@
-  import React from "react";
+import React from "react";
+import { useNavigate } from "react-router";
 import style from "./GroupComponentStyles.module.scss";
 
 export function GroupItem({ group }) {
+  const navigate = useNavigate();
+
+  function navigateToGroup(id) {
+    navigate(`/group_feed/${id}`);
+  }
+
   return (
-    <div className={style.groupItem}>
+    <div className={style.groupItem} onClick={() => navigateToGroup(group.id)}>
       <h3>{group.name}</h3>
       <p>{group.description}</p>
     </div>
