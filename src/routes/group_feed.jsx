bb0643b4cd4377379f323954b9852bc7de0077a4
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2023-03-22 12:04:26 +0100

    added functionality for adding session to group

--- src/routes/group_feed.jsx
+++ src/routes/group_feed.jsx
@@ -1,33 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
-import { getGroupById } from "../api";
+import { addSessionToGroup, getGroupById, getSessions } from "../api";
 import { FeedComponent } from "../components/FeedComponent";
+import { Button, Select } from "../components/Inputs";
 import style from "./group_feed.module.css";
 
 export default function Group_Feed() {
   const params = useParams();
   const [group, setGroup] = useState({});
+  const [groupSessions, setGroupSessions] = useState([]);
   const [sessions, setSessions] = useState([]);
+  const selectedSession = useRef();
 
   useEffect(() => {
     async function getData() {
-      const data = await getGroupById(params.id);
-      setGroup(data);
-      setSessions(data.sessions);
+      const currentGroup = await getGroupById(params.id);
+      const sessions = await getSessions();
+      setGroup(currentGroup);
+      setGroupSessions(currentGroup.sessions);
+      setSessions(sessions);
     }
     getData();
   }, []);
 
+  async function handleAddSessionToGroup(event) {
+    event.preventDefault();
+    await addSessionToGroup(group.id, selectedSession.current.value);
+    console.log(`gruppe: ${group.id}; økt: ${selectedSession.current.value}`);
+    const currentGroup = await getGroupById(params.id);
+    setGroup(currentGroup);
+    setGroupSessions(currentGroup.sessions);
+  }
+
   return (
     <>
       <h1>Feed for {group.name}</h1>
-      <div>
-        {sessions
-          ? sessions.map((session) => (
-              <FeedComponent key={session.id} sessionID={session} />
-            ))
-          : null}
-      </div>
+      <form onSubmit={handleAddSessionToGroup} style={{ padding: "4px 0" }}>
+        <Select
+          reference={selectedSession}
+          name={"session"}
+          id={"session"}
+          options={sessions}
+        />
+        <Button type="submit">Legg ut</Button>
+      </form>
+      {groupSessions
+        ? groupSessions.map((session, idx) => (
+            <FeedComponent key={idx} sessionID={session} />
+          ))
+        : null}
     </>
   );
 }
