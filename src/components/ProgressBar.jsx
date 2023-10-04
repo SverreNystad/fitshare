f45bf3214aa40699eb52b188c0e1b87be0f656ff
Sverre Nystad <sverrenystad@gmail.com>
2023-10-04 10:11:28 +0200

    Refactor: Put all UI into fitshare-ui

--- src/components/ProgressBar.jsx
+++
@@ -1,7 +0,0 @@
-import ProgressBar from 'react-bootstrap/ProgressBar';
-
-function AnimatedExample({ current }) {
-	return <ProgressBar animated now={current} />;
-}
-
-export default AnimatedExample;
--- src/routes/friends.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Friends() {
-  return (
-    <>
-      <div>Venner</div>
-    </>
-  );
-}
